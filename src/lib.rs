def61209cafe503a7fde28b4eb7a6e44cbdaefd9
GeorgeThoppil <georgethoppil2@gmail.com>
2024-08-02 10:49:15 -0400

    adding tokio util for framing bytes

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,11 @@
 mod blockchain;
+mod client;
 mod command;
 mod config;
-mod parse;
 mod server;
 
 pub use blockchain::*;
+pub use client::*;
 pub use command::*;
 pub use config::*;
 pub use server::*;
