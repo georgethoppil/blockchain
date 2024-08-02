d32a707db4af52b0bc3e43de81e66442e2ce27a3
GeorgeThoppil <georgethoppil2@gmail.com>
2024-08-02 14:38:02 -0400

    adding chain functionality

--- src/blockchain/block.rs
+++ src/blockchain/block.rs
@@ -30,9 +30,76 @@ impl Blockchain {
         }
     }
 
-    fn add_block(&mut self) {
-        println!("Adding pending transcations to blockchain");
-        let new_block = Block::new(self.pending_transactions.clone());
-        self.chain.push(new_block);
+    pub fn add_block(&mut self) {
+        if self.pending_transactions.len() > 0 {
+            println!("Adding pending transcations to blockchain");
+            let new_block = Block::new(self.pending_transactions.clone());
+            Self::process_pending_transactions(self);
+            self.chain.push(new_block);
+            self.pending_transactions.clear();
+            println!("State of blockchain {:#?}", self);
+        } else {
+            println!("No pending transactions to add to blockchain");
+        }
+    }
+
+    pub fn get_balance(&self, account: &str) -> Option<u64> {
+        self.accounts.get(account).cloned()
+    }
+
+    pub fn transfer(&mut self, from_account: String, to_account: String, amount: u64) {
+        // check if both accounts exist
+        if !self.accounts.contains_key(&from_account) || !self.accounts.contains_key(&to_account) {
+            println!("account doesn't exist");
+            return;
+        }
+
+        // if there is sufficient amounts
+        if self.accounts[&from_account] < amount {
+            println!("Insufficient amount in the account");
+        }
+
+        let transaction = Transaction {
+            from: Some(from_account),
+            to: Some(to_account),
+            amount,
+            account_id: None,
+        };
+        self.pending_transactions.push(transaction);
+    }
+
+    pub fn create_account(&mut self, id: String, starting_balance: u64) {
+        if self.accounts.contains_key(&id) {
+            println!("Account already exists");
+            return;
+        }
+
+        let transaction = Transaction {
+            from: None,
+            to: None,
+            amount: starting_balance,
+            account_id: Some(id),
+        };
+        self.pending_transactions.push(transaction);
+    }
+
+    fn process_pending_transactions(&mut self) {
+        for transaction in &self.pending_transactions {
+            if transaction.account_id.is_some() {
+                // Account creation
+                if let Some(account_id) = &transaction.account_id {
+                    self.accounts
+                        .insert(account_id.to_string(), transaction.amount);
+                }
+            } else if let (Some(from), Some(to)) = (&transaction.from, &transaction.to) {
+                // Fund transfer
+                if self.accounts.contains_key(from) && self.accounts.contains_key(to) {
+                    if self.accounts[from] >= transaction.amount {
+                        *self.accounts.get_mut(from).unwrap() -= transaction.amount;
+                        *self.accounts.get_mut(to).unwrap() += transaction.amount;
+                    }
+                }
+            }
+        }
     }
 }
